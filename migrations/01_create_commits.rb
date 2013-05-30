deba2a39a9827ecc88d9e624083ebac9e6f4d4de
Mark McGranaghan <mmcgrana@gmail.com>
2013-05-30 01:54:07 +0000

    tighten up initial schema

--- migrations/01_create_commits.rb
+++ migrations/01_create_commits.rb
@@ -2,11 +2,10 @@ Sequel.migration do
   change do
     create_table :commits do
       primary_key :id
-      column :repo_id,    "integer", 
+      column :repo,       "text"
       column :sha,        "text"
       column :additions,  "integer"
       column :deletions,  "integer"
-      column :total,      "integer"
       column :email,      "text"
       column :date,       DateTime
       column :message,    "text"
