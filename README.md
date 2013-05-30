4de7ebf90b96b2b14b2718a6837484f09713a66c
Mark McGranaghan <mmcgrana@gmail.com>
2013-05-30 01:56:12 +0000

    migrate

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ $ heroku addons:add heroku-postgresql:crane
 $ heroku config:add ORGANIZATION="heroku"
 $ heroku config:add IGNORED_REPOS="otp,redistogo"
 $ git push heroku master
+$ heroku run bundle exec bin/migrate
 $ heroku scale runner=1
 
 $ heroku pg:psql
--- lib/commitz/runner.rb
+++ lib/commitz/runner.rb
@@ -8,8 +8,8 @@ require "./lib/commitz/config"
 module Commitz
   module Runner
     def self.start
-      github = Github.new(:basic_auth => Commitz::Config.github_auth, :auto_pagination => true)
-      repo_names = github.repos.list(:org => "heroku").map { |r| r.name }
+      github = Github.new(:basic_auth => Config.github_auth, :auto_pagination => true)
+      repo_names = github.repos.list(:org => Config.organization).map { |r| r.name }
       repo_name = repo_names.first
       puts(repo_name)
     end
